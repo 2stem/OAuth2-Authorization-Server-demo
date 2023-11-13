9852f819b642c1a90713d99788434618ca2fd8b4
최지호 <choijiho@choejihoui-MacBookAir.local>
2023-11-13 21:24:00 +0900

    set InMemoryRegisteredClientRepository bean implementing RegisteredClientRepository for test, and going to change jdbc

--- src/main/java/jiho/oauth2authorizationserverdemo/security/RegisteredClientConfig.java
+++ src/main/java/jiho/oauth2authorizationserverdemo/security/RegisteredClientConfig.java
@@ -7,11 +7,16 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.UUID;
+
+import static org.springframework.security.oauth2.core.ClientAuthenticationMethod.*;
 
 @Configuration
 public class RegisteredClientConfig {
@@ -19,9 +24,14 @@ public class RegisteredClientConfig {
     @Bean
     RegisteredClientRepository registeredClientRepository() {
         var registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("dfegsf")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .clientId("User-service")
+                .clientIdIssuedAt(Instant.now())
+                .clientSecret("{noop}secret")
+                .clientName("User-service")
+                .clientAuthenticationMethod(CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("http://localhost:10001/authorized")
+                .scope("scope-a")
                 .tokenSettings(TokenSettings.builder().accessTokenTimeToLive(Duration.ofHours(2)).build())
                 .build();
 
