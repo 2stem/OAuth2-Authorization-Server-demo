f1c1a4ec3c8c27c771cf80412d2243b572d603cc
twostem <cjh70104@gmail.com>
2023-11-14 01:02:59 +0900

    client authenticationConverter customize to accept only HTTP BASIC

--- src/main/java/jiho/oauth2authorizationserverdemo/security/SecurityConfig.java
+++ src/main/java/jiho/oauth2authorizationserverdemo/security/SecurityConfig.java
@@ -3,14 +3,17 @@ package jiho.oauth2authorizationserverdemo.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
+import org.springframework.security.oauth2.server.authorization.web.authentication.ClientSecretBasicAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -21,7 +24,7 @@ public class SecurityConfig {
     private final OAuth2AuthorizationService authorizationService;
     private final OAuth2AuthorizationConsentService authorizationConsentService;
     private final AuthorizationServerSettings authorizationServerSettings;
-    private final OAuth2TokenGenerator tokenGenerator;
+//    private final OAuth2TokenGenerator tokenGenerator;
 
 
     @Bean
@@ -31,24 +34,32 @@ public class SecurityConfig {
 
         oAuth2AuthorizationServerConfigurer
                 .registeredClientRepository(registeredClientRepository)
-                .authorizationService(authorizationService)
-                .authorizationConsentService(authorizationConsentService)
-                .authorizationServerSettings(authorizationServerSettings)
-                .tokenGenerator(tokenGenerator)
-                .clientAuthentication(clientAuthentication -> { })
-                .authorizationEndpoint(authorizationEndpoint -> { })
-                .deviceAuthorizationEndpoint(deviceAuthorizationEndpoint -> { })
-                .deviceVerificationEndpoint(deviceVerificationEndpoint -> { })
-                .tokenEndpoint(tokenEndpoint -> { })
-                .tokenIntrospectionEndpoint(tokenIntrospectionEndpoint -> { })
-                .tokenRevocationEndpoint(tokenRevocationEndpoint -> { })
-                .authorizationServerMetadataEndpoint(authorizationServerMetadataEndpoint -> { })
-                .oidc(oidc -> oidc
-                        .providerConfigurationEndpoint(providerConfigurationEndpoint -> { })
-                        .logoutEndpoint(logoutEndpoint -> { })
-                        .userInfoEndpoint(userInfoEndpoint -> { })
-                        .clientRegistrationEndpoint(clientRegistrationEndpoint -> { })
-                );
+                .authorizationService(authorizationService) // 일단 default setting (InMemoryAuthorizationService)
+//                .authorizationConsentService(authorizationConsentService)
+//                .authorizationServerSettings(authorizationServerSettings)
+//                .tokenGenerator(tokenGenerator)
+                .clientAuthentication(clientAuthentication -> {
+                    // Client Authentication accepts only HTTP BASIC
+                    clientAuthentication.authenticationConverter(new ClientSecretBasicAuthenticationConverter());
+                })
+//                .authorizationEndpoint(Customizer.withDefaults())
+//                .deviceAuthorizationEndpoint(Customizer.withDefaults())
+//                .deviceVerificationEndpoint(Customizer.withDefaults())
+//                .tokenEndpoint(Customizer.withDefaults())
+//                .tokenIntrospectionEndpoint(Customizer.withDefaults())
+//                .tokenRevocationEndpoint(Customizer.withDefaults())
+//                .authorizationServerMetadataEndpoint(Customizer.withDefaults())
+//                .oidc(oidc -> oidc
+//                        .providerConfigurationEndpoint(providerConfigurationEndpoint -> {
+//                        })
+//                        .logoutEndpoint(logoutEndpoint -> {
+//                        })
+//                        .userInfoEndpoint(userInfoEndpoint -> {
+//                        })
+//                        .clientRegistrationEndpoint(clientRegistrationEndpoint -> {
+//                        })
+//                );
+        ;
 
         return http.build();
     }
